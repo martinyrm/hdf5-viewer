44bbdeaaccb5c52a99a30b0a918d4b615aa46a65
Martin Radulov <me@martinyr.com>
2026-05-18 18:10:34 +0200

    Nearest neighbour GL textures

--- src/main.cpp
+++ src/main.cpp
@@ -1485,8 +1485,8 @@ void upload_heat_texture(FileTab &tab, LoadedDataset &data) {
 
     glGenTextures(1, &tab.heat_texture);
     glBindTexture(GL_TEXTURE_2D, tab.heat_texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
